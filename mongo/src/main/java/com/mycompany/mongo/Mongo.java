d8ba95037ebf61deddafc462651d7bb9b3288226
Niel A. Huaman Cerva <NHuaman@onpe.gob.pe>
2023-02-27 11:59:24 -0500

    test:

--- mongo/src/main/java/com/mycompany/mongo/Mongo.java
+++ mongo/src/main/java/com/mycompany/mongo/Mongo.java
@@ -12,8 +12,18 @@ import org.bson.Document;
  * @author NHuaman
  */
 public class Mongo {
-    
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        // Crear un objeto MongoClient
+        MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
+        // Obtener una base de datos
+        MongoDatabase database = mongoClient.getDatabase("dbmongo");
+        // Obtener la collecion de mongo
+        MongoCollection<Document> collection = database.getCollection("user");
+        // filtar
+        Document user = collection.find(Filters.eq("user", "nhc")).first();
+          // cierra
+        mongoClient.close();
+        System.out.println(user.get("password"));
     }
 }
