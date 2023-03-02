4f5de8e8aab82b30fa5b19275ca66cf4824769d9
Niel A. Huaman Cerva <NHuaman@onpe.gob.pe>
2023-03-02 15:16:09 -0500

    creaciontableview: transmicion controller

---
+++ login/logIng/src/main/java/com/mycompany/loging/score/Util/CreacionTable.java
@@ -0,0 +1,65 @@
+package com.mycompany.loging.score.util;
+
+import com.mycompany.loging.score.model.Actas;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+/**
+ *
+ * @author NHuaman
+ */
+public class CreacionTable {
+
+    public TableView<Actas> viewActas(TableView<Actas> Tactas) {
+        TableColumn<Actas, Integer> columnaActa = new TableColumn<>("acta");
+        columnaActa.setCellValueFactory(new PropertyValueFactory<>("acta"));
+
+        TableColumn<Actas, String> columnaDepartamento = new TableColumn<>("departamento");
+        columnaDepartamento.setCellValueFactory(new PropertyValueFactory<>("departamento"));
+
+        TableColumn<Actas, String> columnaProvincia = new TableColumn<>("provincia");
+        columnaProvincia.setCellValueFactory(new PropertyValueFactory<>("provincia"));
+
+        TableColumn<Actas, String> columnaDistrito = new TableColumn<>("distrito");
+        columnaDistrito.setCellValueFactory(new PropertyValueFactory<>("distrito"));
+
+        TableColumn<Actas, String> columnaHora = new TableColumn<>("hora");
+        columnaHora.setCellValueFactory(new PropertyValueFactory<>("hora"));
+
+        TableColumn<Actas, String> columnaLista = new TableColumn<>("lista");
+        columnaLista.setCellValueFactory(new PropertyValueFactory<>("lista"));
+
+        TableColumn<Actas, String> columnaVotosblancos = new TableColumn<>("votos blancos");
+        columnaVotosblancos.setCellValueFactory(new PropertyValueFactory<>("votosBlanco"));
+
+        TableColumn<Actas, String> columnaVotosNulos = new TableColumn<>("votos nulos");
+        columnaVotosNulos.setCellValueFactory(new PropertyValueFactory<>("votosNulos"));
+
+        TableColumn<Actas, String> columnaVotosInpugnados = new TableColumn<>("votos inpugnados");
+        columnaVotosInpugnados.setCellValueFactory(new PropertyValueFactory<>("votosInpugnados"));
+
+        TableColumn<Actas, String> columnaVotosEmitidos = new TableColumn<>("votos emitidos");
+        columnaVotosEmitidos.setCellValueFactory(new PropertyValueFactory<>("votosEmitidos"));
+
+        TableColumn<Actas, String> columnaObservaciones = new TableColumn<>("observaciones");
+        columnaObservaciones.setCellValueFactory(new PropertyValueFactory<>("observaciones"));
+
+        TableColumn<Actas, String> columnaFirma1 = new TableColumn<>("firma1");
+        columnaFirma1.setCellValueFactory(new PropertyValueFactory<>("firma1"));
+
+        TableColumn<Actas, String> columnaFirma2 = new TableColumn<>("firma2");
+        columnaFirma2.setCellValueFactory(new PropertyValueFactory<>("firma2"));
+
+        TableColumn<Actas, String> columnaFirma3 = new TableColumn<>("firma3");
+        columnaFirma3.setCellValueFactory(new PropertyValueFactory<>("firma3"));
+
+        TableColumn<Actas, String> columnaEstado = new TableColumn<>("estado");
+        columnaEstado.setCellValueFactory(new PropertyValueFactory<>("estado"));
+
+        Tactas.getColumns().addAll(columnaActa, columnaDepartamento, columnaProvincia, columnaDistrito,
+                 columnaHora, columnaLista, columnaVotosblancos, columnaVotosNulos, columnaVotosInpugnados, columnaVotosEmitidos,
+                 columnaObservaciones, columnaFirma1, columnaFirma2, columnaFirma3, columnaEstado);
+        return Tactas;
+    }
+}
---
+++ login/logIng/src/main/java/com/mycompany/loging/score/model/Imagenes.java
@@ -0,0 +1,163 @@
+package com.mycompany.loging.score.model;
+
+/**
+ *
+ * @author NHuaman
+ */
+public class Imagenes {
+
+    String _id;
+    String acta_id;
+    String acta;
+    String departamento;
+    String provincia;
+    String distrito;
+    String hora;
+    String lista1;
+    String votosBlanco;
+    String votosNulos;
+    String votosInpugnados;
+    String votosEmitidos;
+    String observaciones;
+    String firma1;
+    String firma2;
+    String firma3;
+    String estado;
+
+    public String getId() {
+        return _id;
+    }
+
+    public void setId(String _id) {
+        this._id = _id;
+    }
+
+    public String getActa_id() {
+        return acta_id;
+    }
+
+    public void setActa_id(String acta_id) {
+        this.acta_id = acta_id;
+    }
+
+    public String getActa() {
+        return acta;
+    }
+
+    public void setActa(String acta) {
+        this.acta = acta;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    public String getDistrito() {
+        return distrito;
+    }
+
+    public void setDistrito(String distrito) {
+        this.distrito = distrito;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public String getLista1() {
+        return lista1;
+    }
+
+    public void setLista1(String lista1) {
+        this.lista1 = lista1;
+    }
+
+    public String getVotosBlanco() {
+        return votosBlanco;
+    }
+
+    public void setVotosBlanco(String votosBlanco) {
+        this.votosBlanco = votosBlanco;
+    }
+
+    public String getVotosNulos() {
+        return votosNulos;
+    }
+
+    public void setVotosNulos(String votosNulos) {
+        this.votosNulos = votosNulos;
+    }
+
+    public String getVotosInpugnados() {
+        return votosInpugnados;
+    }
+
+    public void setVotosInpugnados(String votosInpugnados) {
+        this.votosInpugnados = votosInpugnados;
+    }
+
+    public String getVotosEmitidos() {
+        return votosEmitidos;
+    }
+
+    public void setVotosEmitidos(String votosEmitidos) {
+        this.votosEmitidos = votosEmitidos;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+
+    public String getFirma1() {
+        return firma1;
+    }
+
+    public void setFirma1(String firma1) {
+        this.firma1 = firma1;
+    }
+
+    public String getFirma2() {
+        return firma2;
+    }
+
+    public void setFirma2(String firma2) {
+        this.firma2 = firma2;
+    }
+
+    public String getFirma3() {
+        return firma3;
+    }
+
+    public void setFirma3(String firma3) {
+        this.firma3 = firma3;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+}
