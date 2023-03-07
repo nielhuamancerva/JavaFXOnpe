d0d70f81102c6ec8b7a735bb188d7f35f920d1a1
Niel A. Huaman Cerva <NHuaman@onpe.gob.pe>
2023-03-07 14:33:05 -0500

    feat:update estructure folder on project

---
+++ login/logIng/src/main/java/com/mycompany/loging/score/Util/VariableGlobales.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.loging.score.util;
+
+import java.util.Map;
+
+/**
+ *
+ * @author NHuaman
+ */
+public class VariableGlobales {
+    
+  public static Map<String,String>  lecturaActasEnMemoria;
+    
+}
