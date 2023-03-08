0a775ffa742fba6c959563cbb5891a58a6811e55
Niel A. Huaman Cerva <NHuaman@onpe.gob.pe>
2023-03-08 14:55:42 -0500

    refactor: tess4jService to optimizacion of software

--- login/logIng/src/main/java/com/mycompany/loging/endpoint/dashboard/RegistrarFirmaController.java
+++ login/logIng/src/main/java/com/mycompany/loging/endpoint/dashboard/RegistrarFirmaController.java
@@ -6,7 +6,6 @@ package com.mycompany.loging.endpoint.dashboard;
 
 import com.mycompany.loging.App;
 
-
 import static com.mycompany.loging.endpoint.dashboard.LeerActasController.acta;
 import com.mycompany.loging.score.Repository.FactoryServiciosExternos;
 import com.mycompany.loging.score.util.DropShadowE;
@@ -29,18 +28,15 @@ import javafx.scene.image.ImageView;
 public class RegistrarFirmaController implements Initializable {
 
     private DropShadowE dropShadowE;
-
+    boolean firmoP;
+    boolean firmoS;
+    boolean firmoT;
     private FactoryServiciosExternos factoryservices;
 
     @FXML
     ImageView firma1, firma2, firma3;
     @FXML
-    private Button btnSiPresi, btnSiSecre, btnSiTercer, btnNoPresi, btnNoSecre, btnNoTercer;
-
-    @FXML
-    private Button btnVerificaTransmision;
-    @FXML
-    private Button btnRegresarObs;
+    private Button btnSiPresi, btnSiSecre, btnSiTercer, btnNoPresi, btnNoSecre, btnNoTercer, btnVerificaTransmision, btnRegresarObs;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -55,9 +51,19 @@ public class RegistrarFirmaController implements Initializable {
 
         factoryservices = FactoryServiciosExternos.getInstance();
         try {
-            factoryservices.Tess4jServiceImpl().validarFirma("FI1-" + VariableGlobales.lecturaActasEnMemoria.get("fileNameSinExtension") + ".png", 150, 3150, 500, 300);
-            factoryservices.Tess4jServiceImpl().validarFirma("FI2-" + VariableGlobales.lecturaActasEnMemoria.get("fileNameSinExtension") + ".png", 804, 3150, 500, 300);
-            factoryservices.Tess4jServiceImpl().validarFirma("FI3-" + VariableGlobales.lecturaActasEnMemoria.get("fileNameSinExtension") + ".png", 1458, 3150, 500, 300);
+            firmoP = factoryservices.Tess4jServiceImpl().validarFirma("FI1-" + VariableGlobales.lecturaActasEnMemoria.get("fileNameSinExtension") + ".png", 150, 3150, 500, 300);
+            firmoS = factoryservices.Tess4jServiceImpl().validarFirma("FI2-" + VariableGlobales.lecturaActasEnMemoria.get("fileNameSinExtension") + ".png", 804, 3150, 500, 300);
+            firmoT = factoryservices.Tess4jServiceImpl().validarFirma("FI3-" + VariableGlobales.lecturaActasEnMemoria.get("fileNameSinExtension") + ".png", 1458, 3150, 500, 300);
+
+            System.out.println(firmoP);
+            System.out.println(firmoS);
+            System.out.println(firmoT);
+            btnSiPresi.setStyle("-fx-background-color: " + (firmoP ? "#2ECC71" : "") + ";");
+            btnNoPresi.setStyle("-fx-background-color: " + (!firmoP ? "" : "") + ";");
+            btnSiSecre.setStyle("-fx-background-color: " + (firmoS ? "#2ECC71" : "") + ";");
+            btnNoSecre.setStyle("-fx-background-color: " + (!firmoS ? "" : "") + ";");
+            btnSiTercer.setStyle("-fx-background-color: " + (firmoT ? "#2ECC71" : "") + ";");
+            btnNoTercer.setStyle("-fx-background-color: " + (!firmoT ? "" : "") + ";");
 
             Image imgfirma1 = new Image(VariableGlobales.lecturaActasEnMemoria.get("FI1-" + VariableGlobales.lecturaActasEnMemoria.get("fileNameSinExtension") + ".png"));
             firma1.setImage(imgfirma1);
@@ -77,8 +83,6 @@ public class RegistrarFirmaController implements Initializable {
         this.dropShadowE = new DropShadowE();
     }
 
-
-
     @FXML
     private void regresarActaVoto() throws IOException {
         App.setRoot(null, "registrarObs");
@@ -86,7 +90,7 @@ public class RegistrarFirmaController implements Initializable {
 
     @FXML
     private void siFirmoPresi(ActionEvent event) {
-        boolean firmoP = true;
+
         btnSiPresi.setStyle("-fx-background-color: " + (firmoP ? "#2ECC71" : "") + ";");
         btnNoPresi.setStyle("-fx-background-color: " + (!firmoP ? "" : "") + ";");
     }
@@ -94,7 +98,6 @@ public class RegistrarFirmaController implements Initializable {
     @FXML
     private void siFirmoSecre(ActionEvent event) {
 
-        boolean firmoS = true;
         btnSiSecre.setStyle("-fx-background-color: " + (firmoS ? "#2ECC71" : "") + ";");
         btnNoSecre.setStyle("-fx-background-color: " + (!firmoS ? "" : "") + ";");
 
@@ -103,7 +106,6 @@ public class RegistrarFirmaController implements Initializable {
     @FXML
     private void siFirmoTercer(ActionEvent event) {
 
-        boolean firmoT = true;
         btnSiTercer.setStyle("-fx-background-color: " + (firmoT ? "#2ECC71" : "") + ";");
         btnNoTercer.setStyle("-fx-background-color: " + (!firmoT ? "" : "") + ";");
 
@@ -112,7 +114,6 @@ public class RegistrarFirmaController implements Initializable {
     @FXML
     private void noFirmoPresi(ActionEvent event) {
 
-        boolean firmoP = true;
         btnNoPresi.setStyle("-fx-background-color: " + (firmoP ? "#2ECC71" : "") + ";");
         btnSiPresi.setStyle("-fx-background-color: " + (!firmoP ? "" : "") + ";");
 
@@ -121,7 +122,6 @@ public class RegistrarFirmaController implements Initializable {
     @FXML
     private void noFirmoSecre(ActionEvent event) {
 
-        boolean firmoS = true;
         btnNoSecre.setStyle("-fx-background-color: " + (firmoS ? "#2ECC71" : "") + ";");
         btnSiSecre.setStyle("-fx-background-color: " + (!firmoS ? "" : "") + ";");
 
@@ -129,7 +129,7 @@ public class RegistrarFirmaController implements Initializable {
 
     @FXML
     private void noFirmoTercer(ActionEvent event) {
-        boolean firmoT = true;
+
         btnNoTercer.setStyle("-fx-background-color: " + (firmoT ? "#2ECC71" : "") + ";");
         btnSiTercer.setStyle("-fx-background-color: " + (!firmoT ? "" : "") + ";");
 
