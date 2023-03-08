94c70cdc6ed75d4fb3d4aa6dc86046d85fd54b88
ricckis <33463381+ricckis@users.noreply.github.com>
2023-03-08 10:50:26 -0500

    ricardo: 08032023 transmision rabbit

--- login/logIng/src/main/java/com/mycompany/loging/endpoint/dashboard/TransmisionRabbitController.java
+++ login/logIng/src/main/java/com/mycompany/loging/endpoint/dashboard/TransmisionRabbitController.java
@@ -6,6 +6,9 @@ package com.mycompany.loging.endpoint.dashboard;
 
 import com.mycompany.loging.App;
 import com.mycompany.loging.score.util.DropShadowE;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -22,6 +25,8 @@ import javafx.scene.control.Button;
  * @author RDeLaCruz
  */
 public class TransmisionRabbitController implements Initializable {
+
+    private static final String QUEUE_NAME = "prueba_cola08"; //nombre de la cola
 
     private DropShadowE dropShadowE;
 
@@ -41,11 +46,27 @@ public class TransmisionRabbitController implements Initializable {
     }
 
     @FXML
-    private void transmitir() throws IOException {
-        Alert alert = new Alert(AlertType.INFORMATION);
-        alert.setTitle("ok");
-        alert.setContentText("Transmision confirmada");
-        alert.showAndWait();
+    private void transmitir() throws Exception {
+
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost("localhost");
+            Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel();
+
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            String message = "primer mensaje";
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+            System.out.println(" [x] Sent '" + message + "'");
+
+            channel.close();
+            connection.close();
+
+            Alert alert = new Alert(AlertType.INFORMATION);
+            alert.setTitle("ok");
+            alert.setContentText("Transmision confirmada");
+            alert.showAndWait();
+
+
     }
 
     @FXML
