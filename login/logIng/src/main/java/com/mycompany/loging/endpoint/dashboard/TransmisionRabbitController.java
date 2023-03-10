b93c699f762b3a5042b868e450723e57efccb40f
Luis Medina Machaca <LMedina@onpe.gob.pe>
2023-03-10 18:57:22 -0500

    fits: Implementacion de transmisionControllerRabbitMQ

--- login/logIng/src/main/java/com/mycompany/loging/endpoint/dashboard/TransmisionRabbitController.java
+++ login/logIng/src/main/java/com/mycompany/loging/endpoint/dashboard/TransmisionRabbitController.java
@@ -15,7 +15,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 
 public class TransmisionRabbitController implements Initializable {
-    private static final String QUEUE_NAME = "cola001"; //nombre de la cola
+    private static final String QUEUE_NAME = "primer_score"; //nombre de la cola
     private DropShadowE dropShadowE;
     @FXML
     private Button btnTransmitir;
@@ -32,20 +32,23 @@ public class TransmisionRabbitController implements Initializable {
     }
     @FXML
     private void transmitir() throws Exception {
-            ConnectionFactory factory = new ConnectionFactory();
-            factory.setHost("localhost");
-            Connection connection = factory.newConnection();
-            Channel channel = connection.createChannel();
-            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-            String message = "primer mensaje";
-            channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
-            System.out.println(" [x] Sent '" + message + "'");
-            channel.close();
-            connection.close();
-            Alert alert = new Alert(AlertType.INFORMATION);
-            alert.setTitle("ok");
-            alert.setContentText("Transmision confirmada");
-            alert.showAndWait();
+             ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("172.16.89.225");
+        factory.setUsername("admin");
+        factory.setPassword("admin");
+        factory.setVirtualHost("/");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        String message = "primer mensaje score XD";
+        channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+        System.out.println(" [x] Sent '" + message + "'");
+        channel.close();
+        connection.close();
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("ok");
+        alert.setContentText("Transmision confirmada");
+        alert.showAndWait();
     }
 
     @FXML
