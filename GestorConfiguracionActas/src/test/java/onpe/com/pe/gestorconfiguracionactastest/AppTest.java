fda7c321597f737c59e4e75a6a25765d695f679f
Niel A. Huaman Cerva <NHuaman@onpe.gob.pe>
2023-03-28 10:56:09 -0500

    refactor: dependecy testunit

--- GestorConfiguracionActas/src/test/java/onpe/com/pe/gestorconfiguracionactastest/AppTest.java
+++ GestorConfiguracionActas/src/test/java/onpe/com/pe/gestorconfiguracionactastest/AppTest.java
@@ -1,70 +1,71 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package onpe.com.pe.gestorconfiguracionactastest;
-
-import java.io.IOException;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.stage.Stage;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.testfx.api.FxAssert;
-import org.testfx.api.FxRobot;
-import org.testfx.framework.junit5.ApplicationExtension;
-import org.testfx.framework.junit5.ApplicationTest;
-import org.testfx.framework.junit5.Start;
-import org.testfx.matcher.base.NodeMatchers;
-
-/**
- *
- * @author NHuaman
- */
-@ExtendWith(ApplicationExtension.class)
-public class AppTest extends ApplicationTest {
-
-
-
-    @Override
-    public void start(Stage stage) throws IOException, Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("onpe/com/pe/gestorconfiguracionactas/login.fxml"));
-        // Cargar la vista FXML de la aplicación
-        Parent root = loader.load();
-
-        // Crear la escena
-        Scene scene = new Scene(root);
-
-        // Establecer la escena en el escenario
-        stage.setScene(scene);
-
-        // Mostrar el escenario
-        stage.show();
-
-        //button = (Button) scene.lookup("#btnIngresar");
-    }
-
-    /*@Test
-  public void testLabel(){
-      Label label = lookup("#").query();
-      assertNotNull(label);
-      assertEquals("Formulario de Acceso",label.getText());
-  }*/
-    @Test
-    public void testButton() {
-        //    assertNotNull(button);
-
-        // Encontrar el botón en la vista
-      //     Button button = lookup("#btnIngresar").queryButton();
-     //   assertNotNull(button);
-
-        //robot.clickOn("");
-        // Verificar que el botón hace lo que se espera
-        //FxAssert.verifyThat("#myLabel", NodeMatchers.hasChild("Button clicked"));*/
-    }
-}
+///*
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+// */
+//package onpe.com.pe.gestorconfiguracionactastest;
+//
+//import java.io.IOException;
+//import javafx.fxml.FXMLLoader;
+//import javafx.scene.Parent;
+//import javafx.scene.Scene;
+//import javafx.scene.control.Button;
+//import javafx.scene.control.Label;
+//import javafx.stage.Stage;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertNotNull;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.testfx.api.FxAssert;
+//import org.testfx.api.FxRobot;
+//import org.testfx.framework.junit5.ApplicationExtension;
+//import org.testfx.framework.junit5.ApplicationTest;
+//import org.testfx.framework.junit5.Start;
+//import org.testfx.matcher.base.NodeMatchers;
+//
+///**
+// *
+// * @author NHuaman
+// */
+///*
+//@ExtendWith(ApplicationExtension.class)
+//public class AppTest extends ApplicationTest {
+//
+//
+//
+//    @Override
+//    public void start(Stage stage) throws IOException, Exception {
+//        FXMLLoader loader = new FXMLLoader(getClass().getResource("onpe/com/pe/gestorconfiguracionactas/login.fxml"));
+//        // Cargar la vista FXML de la aplicación
+//        Parent root = loader.load();
+//
+//        // Crear la escena
+//        Scene scene = new Scene(root);
+//
+//        // Establecer la escena en el escenario
+//        stage.setScene(scene);
+//
+//        // Mostrar el escenario
+//        stage.show();
+//
+//        //button = (Button) scene.lookup("#btnIngresar");
+//    }
+//
+//    /*@Test
+//  public void testLabel(){
+//      Label label = lookup("#").query();
+//      assertNotNull(label);
+//      assertEquals("Formulario de Acceso",label.getText());
+//  }*//*
+//    @Test
+//    public void testButton() {
+//        //    assertNotNull(button);
+//
+//        // Encontrar el botón en la vista
+//      //     Button button = lookup("#btnIngresar").queryButton();
+//     //   assertNotNull(button);
+//
+//        //robot.clickOn("");
+//        // Verificar que el botón hace lo que se espera
+//        //FxAssert.verifyThat("#myLabel", NodeMatchers.hasChild("Button clicked"));*/
+//   /* }
+//}*/
