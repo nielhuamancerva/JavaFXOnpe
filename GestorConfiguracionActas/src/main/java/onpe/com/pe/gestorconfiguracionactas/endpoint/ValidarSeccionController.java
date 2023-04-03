5c504a5a129856692e298094d00144c5210a2af8
ricckis <33463381+ricckis@users.noreply.github.com>
2023-04-02 23:58:01 -0500

    feat: validar seccion

--- GestorConfiguracionActas/src/main/java/onpe/com/pe/gestorconfiguracionactas/endpoint/ValidarSeccionController.java
+++ GestorConfiguracionActas/src/main/java/onpe/com/pe/gestorconfiguracionactas/endpoint/ValidarSeccionController.java
@@ -21,20 +21,18 @@ import onpe.com.pe.gestorconfiguracionactas.App;
  *
  * @author rdela
  */
-public class ConfiguraSeccionesController implements Initializable {
+public class ValidarSeccionController implements Initializable {
 
     @FXML
-    private Button btnRegresar;
-    @FXML
-    private Button btnGuardar;
-    @FXML
     private ComboBox<?> cboDocumentos;
-    @FXML
-    private Button ico_activar;
     @FXML
     private ScrollPane scrollPaneActa;
     @FXML
     private ImageView imgViewActa;
+    @FXML
+    private Button btnFinaliza;
+    @FXML
+    private Button btnRegresar;
 
     /**
      * Initializes the controller class.
@@ -45,18 +43,13 @@ public class ConfiguraSeccionesController implements Initializable {
     }    
 
     @FXML
-    private void actionRegresar() throws IOException{
-        App.setRoot(null, "configurationDoc");
+    private void actionFinaliza(ActionEvent event) {
     }
 
     @FXML
-    private void actionGuardar(ActionEvent event) {
-    }
-
-    private void actionContinuar()throws IOException{
-        App.setRoot(null, "validarSeccion");
+    private void actionRegresar() throws IOException{
         
+        App.setRoot(null, "configuraSecciones");
     }
-
     
 }
